dc378639571cdeb2f8a90e042cd5f84a2a62d1b8
bryant tunbutr <bryantt23@users.noreply.github.com>
2022-11-27 22:35:08 -0800

    set sign

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,25 @@
 import horoscopesObj from '../data/horoscopes';
+import { useContext } from 'react';
+import { HoroscopeContext } from '../context/HoroscopeContext';
 
 const Navbar = () => {
   const horoscopes = Object.keys(horoscopesObj);
+  const { setCurrentSign } = useContext(HoroscopeContext);
 
   return (
     <nav>
       {horoscopes.map(sign => (
-        <span key={sign}>
-        {sign}
+        <span
+          key={sign}
+          onClick={e => {
+            setCurrentSign(sign);
+          }}
+        >
+          {sign}
         </span>
       ))}
     </nav>
-  )
+  );
 };
 
 export default Navbar;
