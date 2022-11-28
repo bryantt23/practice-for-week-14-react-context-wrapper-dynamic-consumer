2b24a6bb2437212e4f31f9bfbaff2ac576471007
bryant tunbutr <bryantt23@users.noreply.github.com>
2022-11-27 22:49:02 -0800

    show match, which is independent of component state

--- src/components/Match.js
+++ src/components/Match.js
@@ -2,7 +2,15 @@ import { useContext, useState } from 'react';
 import { HoroscopeContext } from '../context/HoroscopeContext';
 
 function Match() {
-  return <div>Match</div>;
+  const [match, setMatch] = useState();
+  const { sign } = useContext(HoroscopeContext);
+  console.log('🚀 ~ file: Match.js ~ line 7 ~ Match ~ sign', sign);
+  return (
+    <div>
+      <button onClick={() => setMatch(!match)}>Show Match</button>
+      {match && <p>Match is: {sign.match}</p>}
+    </div>
+  );
 }
 
 export default Match;
