2fa1a42128559bde0b3953a3d7997013948aba8f
bryant tunbutr <bryantt23@users.noreply.github.com>
2022-11-27 22:41:19 -0800

    match component

---
+++ src/components/Match.js
@@ -0,0 +1,8 @@
+import { useContext, useState } from 'react';
+import { HoroscopeContext } from '../context/HoroscopeContext';
+
+function Match() {
+  return <div>Match</div>;
+}
+
+export default Match;
